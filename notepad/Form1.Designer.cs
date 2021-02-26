0faed6dad8abbc52b2f08afabb00184db6896843
Byyyxd <bysecundario8@gmail.com>
2021-02-25 20:53:04 -0500

    notepad

---
+++ notepad/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Label();
+            this.drag = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.github = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(256, 202);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "- wszz#2021";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveBtn.ForeColor = System.Drawing.Color.White;
+            this.saveBtn.Location = new System.Drawing.Point(12, 220);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(64, 27);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loadBtn.ForeColor = System.Drawing.Color.White;
+            this.loadBtn.Location = new System.Drawing.Point(196, 220);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(72, 27);
+            this.loadBtn.TabIndex = 2;
+            this.loadBtn.Text = "Load Text";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // exit
+            // 
+            this.exit.AutoSize = true;
+            this.exit.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.ForeColor = System.Drawing.Color.White;
+            this.exit.Location = new System.Drawing.Point(266, 1);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(15, 13);
+            this.exit.TabIndex = 3;
+            this.exit.Text = "X";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // drag
+            // 
+            this.drag.TargetControl = this;
+            // 
+            // github
+            // 
+            this.github.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.github.ForeColor = System.Drawing.Color.White;
+            this.github.Location = new System.Drawing.Point(82, 220);
+            this.github.Name = "github";
+            this.github.Size = new System.Drawing.Size(108, 27);
+            this.github.TabIndex = 4;
+            this.github.Text = "Github";
+            this.github.UseVisualStyleBackColor = true;
+            this.github.Click += new System.EventHandler(this.github_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(280, 252);
+            this.Controls.Add(this.github);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.loadBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.Label exit;
+        private Siticone.UI.WinForms.SiticoneDragControl drag;
+        private System.Windows.Forms.Button github;
+    }
+}
+
---
+++ notepad/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog f = new SaveFileDialog();
+            f.Filter = "(*.txt)|*.txt";
+            f.FileName = "wszz.txt";
+            f.InitialDirectory = Application.ExecutablePath;
+            if(f.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(Path.GetFullPath(f.FileName), richTextBox1.Text);
+            }
+        }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            f.Filter = "(*.txt)|*.txt";
+            if(f.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(Path.GetFullPath(f.FileName));
+            }
+        }
+
+        private void github_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/byyyxd");
+        }
+    }
+}
